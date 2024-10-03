ffd2098915f2b2cb88aa052aec432d48028faf40
moon <undruee@gmail.com>
2024-10-03 14:57:39 +0300

    create_table

--- src/func_sql/begin.c
+++ src/func_sql/begin.c
@@ -4,13 +4,42 @@ void create(void) {
   char name_db[100] = {0};
   int exit;
   sqlite3 *db;
-  printf("Введите имя новой таблицы: ");
+  printf("Введите имя новой базы данных: ");
   scanf("%99s", name_db);
   exit = sqlite3_open(name_db, &db);
   if (exit) {
-    fprintf(stderr, "Ошиббка при создании таблицы: %s\n", sqlite3_errmsg(db));
+    fprintf(stderr, "Ошиббка при создании базы данных: %s\n",
+            sqlite3_errmsg(db));
   } else {
-    fprintf(stderr, "Таблица успешно создана\n");
+    fprintf(stderr, "База данных успешно создана\n");
+  }
+  sqlite3_close(db);
+}
+
+void create_table(void) {
+  const char *sql = "CREATE TABLE IF NOT EXISTS Users ("
+                    "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    "Name TEXT NOT NULL,"
+                    "Age INTEGER NOT NULL);";
+  char name_db[100] = {0};
+  char *errMsg = 0;
+  int exit;
+  sqlite3 *db;
+  int flag;
+  printf("Введите имя базы данных: ");
+  scanf("%99s", name_db);
+  exit = sqlite3_open(name_db, &db);
+  if (exit) {
+    fprintf(stderr, "Ошибка при открытии базы данных: %s\n",
+            sqlite3_errmsg(db));
+  } else {
+    fprintf(stderr, "База данныз успешно открыта!\n");
+  }
+  flag = sqlite3_exec(db, sql, 0, 0, &errMsg);
+  if (flag != SQLITE_OK) {
+    fprintf(stderr, "Ошибка при создании таблицы: %s\n", errMsg);
+  } else {
+    printf("Таблица успешно создана!\n");
   }
   sqlite3_close(db);
 }
--- src/func_sql/begin.h
+++ src/func_sql/begin.h
@@ -1,6 +1,7 @@
 #ifndef BEGIN_H
 #define BEGIN_H
 void create(void);
+void create_table(void);
 #endif
 #ifndef STDIO_STRING_SQLITE_H
 #define STDIO_STRING_SQLITE_H
